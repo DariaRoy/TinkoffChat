f48c2076b3a3fa91550a63b68c10ab32876814dc
Dasha <dasha02.08@mail.ru>
2017-09-21 13:41:40 +0300

    1 lesson

--- Chat/AppDelegate.swift
+++ Chat/AppDelegate.swift
@@ -15,13 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+//        window = UIWindow.init(frame: UIScreen.main.bounds)
+//        if let keyWindow = window {
+//            keyWindow.rootViewController = ProfileViewController()
+//            keyWindow.makeKeyAndVisible()
+//        }
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.    calls
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
