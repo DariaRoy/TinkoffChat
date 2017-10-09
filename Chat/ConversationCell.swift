7761029ce9c06675f2d3759eeb311bcdf9bdd02c
Dasha <dasha02.08@mail.ru>
2017-10-09 07:16:50 +0300

    homework 3

--- Chat/ConversationCell.swift
+++ Chat/ConversationCell.swift
@@ -22,42 +22,15 @@ struct ConversationCell {
     var hasUnreadMessages: Bool
     
     func dateInString() -> String {
-        let current = Date(timeIntervalSinceNow: 0)
-        return getDate(date: getDateTuple(date: self.date!), current: getDateTuple(date: current))
+        if NSCalendar.current.isDateInToday(date!) {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm"
+            return formatter.string(from: date!)
+        } else {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd MMM"
+            return formatter.string(from: date!)
+        }
     }
-    
 }
 
-
-func getDateTuple(date: Date) -> (year: Int, month: Int, day: Int, hour: Int,minute: Int, monthMMM: String) {
-    
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss +zzzz"
-    
-    let monthFormatter = DateFormatter()
-    monthFormatter.dateFormat = "MMM"
-    let monthMMM = monthFormatter.string(from: date)
-    
-    let calendar = Calendar.current
-    let comp = calendar.dateComponents([.hour, .minute, .day, .month, .year], from: date)
-    let day = comp.day!
-    let month = comp.month!
-    let year = comp.year!
-    let hour = comp.hour!
-    let minute = comp.minute!
-    
-    return (year, month, day, hour, minute, monthMMM)
-    
-}
-
-
-func getDate (date: (year: Int, month: Int, day: Int, hour: Int, minute: Int, monthMMM: String), current: (year: Int, month: Int, day: Int, hour: Int,minute: Int, monthMMM: String) ) -> String {
-    
-    if date.year < current.year || date.year == current.year && date.month < current.month || date.year == current.year && date.month == current.month && date.day < date.day {
-        return "\(date.day) \(date.monthMMM)"
-    } else {
-        let zero = date.minute < 10 ? "0" : ""
-        return "\(date.hour):\(zero)\(date.minute)"
-    }
-    
-}
